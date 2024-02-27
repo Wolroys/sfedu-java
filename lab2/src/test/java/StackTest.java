0c58d4e5b87cf3040f16627cd9dfd512cd806ada
Aleksey Paslenov <alekseypaslenov@gmail.com>
2024-02-27 22:23:54 +0300

    added tests and bracket checks

--- lab2/src/test/java/StackTest.java
+++ lab2/src/test/java/StackTest.java
@@ -35,4 +35,61 @@ public class StackTest {
 
         assertThrows(RuntimeException.class, stack::pop);
     }
+
+    @Test
+    void bracketsShouldBeCorrect(){
+        assertTrue(isBracketsBalanced("({[]})"));
+
+        assertFalse(isBracketsBalanced("({[}])"));
+
+        assertTrue(isBracketsBalanced("/* begin end */"));
+
+        assertFalse(isBracketsBalanced("/* begin end */ {"));
+    }
+
+    boolean isBracketsBalanced(String str) {
+        CustomStack<String> stack = new CustomStack<>();
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            switch (c) {
+                case '(':
+                case '{':
+                case '[':
+                case '<':
+                case 'b':
+                    stack.push(String.valueOf(c));
+                    break;
+                case '/':
+                    if (i < str.length() - 1 && str.charAt(i + 1) == '*') {
+                        stack.push("/*");
+                        i++;
+                    }
+                    break;
+                case ')':
+                    if (stack.isEmpty() || !stack.pop().equals("(")) return false;
+                    break;
+                case '}':
+                    if (stack.isEmpty() || !stack.pop().equals("{")) return false;
+                    break;
+                case ']':
+                    if (stack.isEmpty() || !stack.pop().equals("[")) return false;
+                    break;
+                case '>':
+                    if (stack.isEmpty() || !stack.pop().equals("<")) return false;
+                    break;
+                case '*':
+                    if (i < str.length() - 1 && str.charAt(i + 1) == '/' && (!stack.isEmpty() &&
+                            stack.pop().equals("/*"))) {
+                        i++;
+                    } else {
+                        return false;
+                    }
+                    break;
+                case 'd':
+                    if (stack.isEmpty() || !stack.pop().equals("b")) return false;
+                    break;
+            }
+        }
+        return stack.isEmpty();
+    }
 }
