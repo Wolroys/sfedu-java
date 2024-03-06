d3add5009f941946e7ffadf5efecaeb673217aeb
Aleksey Paslenov <alekseypaslenov@gmail.com>
2024-03-06 22:33:20 +0300

    created class Pont

---
+++ lab6/src/main/java/com/sfedu/Point.java
@@ -0,0 +1,40 @@
+package com.sfedu;
+
+public class Point<T extends Number> {
+
+    private T x;
+    private T y;
+
+    public Point(T x, T y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public T getX() {
+        return x;
+    }
+
+    public void setX(T x) {
+        this.x = x;
+    }
+
+    public T getY() {
+        return y;
+    }
+
+    public void setY(T y) {
+        this.y = y;
+    }
+
+    public double distance() {
+        return Math.sqrt(Math.pow(x.doubleValue(), 2) + Math.pow(y.doubleValue(), 2));
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+}
