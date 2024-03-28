d50702e710231df7eb9a7b1c88e3dbe93f1ceab7
Aleksey <alekseypaslenov@gmail.com>
2024-03-28 20:08:37 +0300

    finished 5 lab

--- lab5/src/main/java/com/sfedu/Dictionary.java
+++ lab5/src/main/java/com/sfedu/Dictionary.java
@@ -1,4 +1,29 @@
 package com.sfedu;
 
+import java.util.*;
+
 public class Dictionary {
+    private final Map<String, Set<String>> synonyms;
+
+    public Dictionary() {
+        synonyms = new HashMap<>();
+    }
+
+    public void add(String word1, String word2) {
+        synonyms.putIfAbsent(word1, new HashSet<>());
+        synonyms.putIfAbsent(word2, new HashSet<>());
+
+        synonyms.get(word1).add(word2);
+        synonyms.get(word2).add(word1);
+    }
+
+    @SuppressWarnings("unchecked")
+    public int countSynonyms(String word) {
+        return synonyms.getOrDefault(word, Collections.EMPTY_SET).size();
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<String> getSynonyms(String word) {
+        return synonyms.getOrDefault(word, Collections.EMPTY_SET).stream().toList();
+    }
 }
